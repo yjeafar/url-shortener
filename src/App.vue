94ea6e3974d50032fc7ab2d2050db1ae1e6abecb
Yasser Jeafar <yjeafar@gmail.com>
2020-10-29 22:30:32 -0400

    Added Express and created new page
    Created url shortener page and started creating the front end. Added express so I could begin setting the backend up

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,13 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/url-shortener.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    UrlShortener
   }
 }
 </script>
---
+++ src/components/url-shortener.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="url-shortner">
+    <h1>{{ msg }}</h1>
+    <p>
+     This page allows for you to input a url and it will automatically be shortened.
+    </p>
+    <div>
+        <input v-model="inputUrl" placeholder="URL you'd like to shorten here">
+        <p>Input URL is: {{ inputUrl }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UrlShortener',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  margin: 40px 0 0;
+}
+
+</style>
