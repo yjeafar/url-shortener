31d1652e404072da76a1ce852b7a94ca296b857a
Yasser Jeafar <yjeafar@gmail.com>
2020-11-06 22:22:32 -0500

    Removed home page and added my own
    Added home page on previous application so it now looks identical but the routing now works properly. Had an issue setting up the first project which is why I had to recreate it with customer additions

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,54 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+<title> Url Shortener </title>
+  <div class="urlShortener">
+    <h1>{{ url }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+     This page allows for you to input a url and it will automatically be shortened.
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div>
+      <input id="urlText" v-model="inputUrl" placeholder="URL to Shorten">
+     <button type="button" id="submitButton" class="btn btn-primary">Submit</button>
+      <p style="padding-top: 25px">Input URL is: {{ inputUrl }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Home',
   props: {
-    msg: String,
+    url: String,
+  },
+  data() {
+    return {
+      inputUrl: '',
+    };
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
+div {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.urlShortener {
+  color: dodgerblue;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+#urlText {
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  border-radius: 5px;
+  border-color: silver;
+  outline: none;
+  border-style: solid;
+  padding: 3px;
 }
-a {
-  color: #42b983;
+
+#submitButton {
+  margin-left: 15px;
+  margin-bottom: 4px;
 }
 </style>
